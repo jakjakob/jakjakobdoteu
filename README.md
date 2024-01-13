52750ef6b49ee0cd16263c487950bdf3b671300b
jakjakob <118139800+jakjakob@users.noreply.github.com>
2024-01-13 17:16:18 +0100

    x

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Note hat the website is work in progress.
 
 TO DO
 
+fix footer height only getting checked when resizing and not on page load
+
 would also make sure to include some projects & other things you're proud of on your home page!
 
 -- put /before links in error pages
--- public_www/scripts/main.js
+++ public_www/scripts/main.js
@@ -1,6 +1,7 @@
 //for min height and center item
-window.addEventListener('resize', function() {
+function footerheight(){
     let foot_height=document.getElementById("footer").clientHeight +"px";
-var r = document.querySelector(':root');
-r.style.setProperty("--footer-height", foot_height);
-});
+    var r = document.querySelector(':root');
+    r.style.setProperty("--footer-height", foot_height);
+}
+window.addEventListener('resize', function() {footerheight();});
